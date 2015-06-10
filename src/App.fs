0dbb7c62cc1c9cedfcb34ea77ae06b986c53df2a
Valeriu Caraulean <valeriu@caraulean.com>
2015-06-11 00:25:59 +0200

    On my way trough Chapter 5. Ended with Genre list

--- src/App.fs
+++ src/App.fs
@@ -18,10 +18,17 @@ let browse =
         | Choice2Of2 msg -> BAD_REQUEST msg
     )
 
+let overview = warbler (fun _ ->
+    Db.getContext()
+    |> Db.getGenres
+    |> List.map (fun g -> g.Name)
+    |> View.store
+    |> html)
+
 let webPart = 
     choose [
         path Path.home >>= html View.home
-        path Path.Store.overview >>= html (View.store ["Rock"; "Jazz"; "Pop"])
+        path Path.Store.overview >>= overview
         path Path.Store.browse >>= browse
         pathScan Path.Store.details (fun id -> html (View.details id))
 
